82459919f94ae7f5d919bd36d6e2b77ade31ab9e
Keke Pradipta <kekedp11@gmail.com>
2026-07-03 20:14:54 +0700

    feat: add footer section

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,48 @@
+import "../../styles/Footer.css";
+
+import {
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+
 function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="footer">
+      <div className="container footer-container">
+
+        <h2 className="footer-logo">
+          KP
+        </h2>
+
+        <p className="footer-text">
+          Built with React, Node.js, Express, and MongoDB.
+        </p>
+
+        <div className="footer-socials">
+
+          <a
+            href="https://github.com/kekedp11"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/keke-pradipta-913b39184"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+
+        </div>
+
+        <p className="footer-copy">
+          © 2026 Keke Pradipta. All rights reserved.
+        </p>
+
+      </div>
     </footer>
   );
 }
